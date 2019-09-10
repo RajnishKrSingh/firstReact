08c0a2da672533440846271a124f0a35970c5987
Rajnish Kr Singh <rajnishsinghrs344@gmail.com>
2019-09-10 21:51:00 +0530

    Updates

---
+++ precache-manifest.3b80cd6bbf735ebe939b6e9cf73b5d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369d0be3ff785973d2bdfe4469d24c49",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "cb98d8e816a15194c6dd",
+    "url": "/firstReact/static/css/main.32c36e4c.chunk.css"
+  },
+  {
+    "revision": "b82b21863b0d2f5b97b2",
+    "url": "/firstReact/static/js/2.804bdad5.chunk.js"
+  },
+  {
+    "revision": "cb98d8e816a15194c6dd",
+    "url": "/firstReact/static/js/main.e8551385.chunk.js"
+  },
+  {
+    "revision": "82639a371aaefaf951f7",
+    "url": "/firstReact/static/js/runtime~main.23d03f9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
